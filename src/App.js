e3193eb196e76c2239acaaf8eeb113c7b951b071
nastacosta <go.acosta3@gmail.com>
2023-08-21 18:13:17 -0300

    elemento para titulos añadido

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
-import AboutMe from './components/AboutMe';
-import Header from './components/Header';
-import Landing from './components/Landing';
+import "./App.css";
+import AboutMe from "./components/AboutMe";
+import Header from "./components/Header";
+import Landing from "./components/Landing";
 
 function App() {
   return (
     <body className="app">
-      <section className='landing-frame'>
+      <section className="landing-frame">
         <Header></Header>
         <Landing></Landing>
       </section>
+      <div className="title-box">
+        <h3>./SOBREMI</h3>
+        <h2>Una breve descripción de mi persona</h2>
+      </div>
       <AboutMe></AboutMe>
     </body>
   );
