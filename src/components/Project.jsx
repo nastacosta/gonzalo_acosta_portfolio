d3af86360c378d9bd7a4640ad627711aedda77fd
nastacosta <go.acosta3@gmail.com>
2023-08-22 15:00:47 -0300

    Finalizado el estilo del template de proyecto

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,25 +2,27 @@ function Project() {
   return (
     <article className="project-article">
       <img
-        className="project-photo"
+        className="project-image"
         src="/img/test-project-image.png"
         alt="imagen del proyecto"
       />
-      <section>
-        <h3>Sportit Project</h3>
-        <div>
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
+      <section className="project-about">
+        <h3 className="primary-subheading">Sportit Project</h3>
+        <div className="project-tech-used">
+          <img src="/img/html-logo.png" alt="" />
+          <img src="/img/css-logo.png" alt="" />
+          <img src="/img/react-logo.png" alt="" />
         </div>
         <p className="primary-text">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
           quidem quos debitis voluptatibus modi consequuntur architecto quas qui
           totam ipsa aperiam voluptas eius laudantium, consectetur aliquid omnis
-          doloribus eveniet quod? qui totam ipsa aperiam voluptas
+          eius laudantium,
         </p>
-        <img src="/img/github-logo-black.png" alt="github link" />
-        <img src="/img/link.png" alt="project link" />
+        <div className="project-links">
+          <img src="/img/github-logo-black.png" alt="github link" />
+          <img src="/img/link.png" alt="project link" />
+        </div>
       </section>
     </article>
   );
